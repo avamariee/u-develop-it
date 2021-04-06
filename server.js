6d3ef2310fccd296f7a052a83bb5c6e2d31bfd84
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-04-05 19:59:10 -0600

    made query to the server

--- server.js
+++ server.js
@@ -1,21 +1,49 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+
+
 
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Conect to mySQL database >> 
 
-app.get('/', (req,res) => {
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+
+        // Your  MySQL username,
+        user: 'root',
+
+        // Your MySQL Password
+        password: 'Bonjovilol1!2!3!',
+
+        database: 'election'
+
+    },
+
+    console.log('Connected to the election database.')
+);
+
+
+app.get('/', (req, res) => {
     res.json({
         message: 'Hello World.'
     });
 });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+
+
 // default response for any other request (NOT FOUND) >>
 // make sure this is the last route or it will override all of the other routes
-app.use((req,res) => {
+app.use((req, res) => {
     res.status(404).end();
 })
 
